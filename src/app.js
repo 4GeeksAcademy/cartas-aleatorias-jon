84d7e9e7161541a886a0ec5ff795d1555b5e06c1
Jonurroz <aitorurroz@gmail.com>
2025-04-10 12:09:37 +0000

    random-card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 import "bootstrap";
 import "./style.css";
 
+function deal() {
+  
+  let cardNum = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  let palos = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  let cardRandomNum = Math.floor(Math.random() * cardNum.length);
+  let cardRandomPalos = Math.floor(Math.random() * palos.length);
+  let paloFinal = palos[cardRandomPalos];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.getElementById('content').innerHTML = cardNum[cardRandomNum];
+    document.getElementById('card-content').className = "";
+    document.getElementById('card-content').classList.add("the-card");
+    document.getElementById('card-content').classList.add(getSuiteClass(paloFinal));
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+}
+
+window.getDeal = deal;
+
+window.onload = deal;
+
+function getSuiteClass(palos) {
+  switch (palos) {
+    case "Diamonds": return "palo-diamonds";
+    case "Spades": return "palo-spades";
+    case "Hearts": return "palo-hearts";
+    case "Clubs": return "palo-clubs";
+  }
+}
+
+
+ 
+ 
